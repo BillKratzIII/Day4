1b6cf878c14621bba3277c7766a1c23746953665
BillKratzIII <wkratz3@gmail.com>
2016-08-05 08:26:36 -0400

    8/5/16 8:26am Fixed error on single letter words. Added if statement to handle one letter words.

--- src/day4work/PigLatin.java
+++ src/day4work/PigLatin.java
@@ -7,7 +7,7 @@ public class PigLatin {
 
 	public static void main(String[] args) {
 		String userInput = getUserInput();
-		System.out.println("The word " + userInput + " in pig latin is " + translator(userInput));
+		translator(userInput);
 	}
 	
 	public static String getUserInput(){
@@ -15,50 +15,72 @@ public class PigLatin {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Please enter 1 word and press enter:");
+		System.out.println("Enter your phrase to be translated to Pig Latin:");
 		userInput = sc.nextLine();
 		sc.close();
 		return userInput;
 	}
 
-	public static String translator(String wordToBeTranslated){
+	public static void translator(String phraseToBeTranslated){
+		String phrase = phraseToBeTranslated;
+		String[] words = phrase.split("\\s");
+		
+		for(int i=0; i<words.length; i++){
+			System.out.println(words[i]);
+		}
+		
+		for(int i=0; i<words.length; i++){
+			System.out.print(translateWord(words[i]) + " ");
+		}
+		
+	}
+	
+	public static String translateWord(String wordToBeTranslated){
 		String word = wordToBeTranslated;
 		String pigLatin = null;
 		int length = word.length();
 		char firstLetter;
 		char secondLetter;
-		//char[] constArray = new char[length];
 		char[] tempArray = new char[length+4];
 		
 		StringBuilder sb = new StringBuilder(word);
 		firstLetter = sb.charAt(0);
-		secondLetter = sb.charAt(1);
-		
-		if (isVowel(firstLetter) == false){
-			if(isVowel(secondLetter) == true){
-				sb.deleteCharAt(0);//delete first character in string builder
-				//transfer remaining characters to a char array
-				for(int i=0; i<length-1; i++){
-					tempArray[i] = sb.charAt(i);
-				}
-				tempArray[length-1] = '-';
-				tempArray[length] = firstLetter;
-				tempArray[length+1] = 'a';
-				tempArray[length+2] = 'y';
-				pigLatin = new String(tempArray);
-			}else{
-				pigLatin = constCluster(word);
-			}
-		
-		}else {
-			for (int i=0; i<length; i++){
-				tempArray[i] = sb.charAt(i);
-			}
+		if (word.length() == 1){
+			tempArray[0] = firstLetter;
 			tempArray[length] = '-';
 			tempArray[length+1] = 'y';
 			tempArray[length+2] = 'a';
 			tempArray[length+3] = 'y';
 			pigLatin = new String(tempArray);
+		}else{
+			secondLetter = sb.charAt(1);
+		
+			if (isVowel(firstLetter) == false){
+				if(isVowel(secondLetter) == true){
+					sb.deleteCharAt(0);//delete first character in string builder
+					//transfer remaining characters to a char array
+					for(int i=0; i<length-1; i++){
+						tempArray[i] = sb.charAt(i);
+					}
+					tempArray[length-1] = '-';
+					tempArray[length] = firstLetter;
+					tempArray[length+1] = 'a';
+					tempArray[length+2] = 'y';
+					pigLatin = new String(tempArray);
+				}else{
+					pigLatin = constCluster(word);
+				}
+		
+			}else {
+				for (int i=0; i<length; i++){
+					tempArray[i] = sb.charAt(i);
+				}
+				tempArray[length] = '-';
+				tempArray[length+1] = 'y';
+				tempArray[length+2] = 'a';
+				tempArray[length+3] = 'y';
+				pigLatin = new String(tempArray);
+			}
 		}
 		return pigLatin;
 	}
@@ -77,8 +99,6 @@ public class PigLatin {
 	
 	public static String constCluster(String wordToBeTranslated){
 		int length = wordToBeTranslated.length();
-		//char[] firstPart = new char[length];
-		//char[] secondPart = new char[length];
 		String word = wordToBeTranslated;
 		String pigLatin = null;
 		StringBuilder sb = new StringBuilder(word);
